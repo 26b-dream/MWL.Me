f4f9bc0e37b56a14af66ce352a1a62f4f34bf622
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-07-27 06:24:18 -0700

    Added quick and dirty manga support

--- main/views.py
+++ main/views.py
@@ -39,20 +39,24 @@ def json_response(request: HttpRequest) -> HttpResponse:
     if not form.is_valid():
         return HttpResponse("Invalid form")
 
+    media_type = form.cleaned_data["anime_or_manga"]
+
     command: list[str] = ["out"]
     user = MyAnimeListUser(form.cleaned_data["username"])
 
     use_values = [value for key, value in USE_CROSSREF.items() if form.cleaned_data.get(key) == True]
-    status_in = f"""user_anime.status IN ("{'", "'.join(use_values)}'")"""
+    status_in = f"""user_{media_type}.status IN ("{'", "'.join(use_values)}'")"""
 
     do_not_return_values = [v for k, v in DO_NOT_RETURN_CROSSREF.items() if not form.cleaned_data.get(k)]
     # If anime/manga with a specific status are set to not be returned use a more complex query
     if do_not_return_values:
         return_string = '", "'.join(do_not_return_values)
         if form.cleaned_data.get("do_not_return_not_on_list"):
-            return_string = f'AND (rec_user_anime.status IS NULL or rec_user_anime.status NOT IN ("{return_string}"))'
+            return_string = (
+                f'AND (rec_user_{media_type}.status IS NULL or rec_user_{media_type}.status NOT IN ("{return_string}"))'
+            )
         else:
-            return_string = f'AND (rec_user_anime.status NOT IN ("{return_string}"))'
+            return_string = f'AND (rec_user_{media_type}.status NOT IN ("{return_string}"))'
 
     # If all anime/manga are to be returned a simpler query can be used
     else:
@@ -65,18 +69,20 @@ def json_response(request: HttpRequest) -> HttpResponse:
     # TODO: Technically not every query needs the IIF statement
     # TODO: Including it just makes things easier to read and write
     # TODO: Find a clean way to split this up better for faster queries
-    select_rec_score_string = f"IIF(anime_recs.recommendations > {recs_to_truncate}, {recs_to_truncate}"
+    select_rec_score_string = f"IIF({media_type}_recs.recommendations > {recs_to_truncate}, {recs_to_truncate}"
     # If popularity compensation is enabled use an extra math string to compensate
     if request.GET.get("popularity_compensation"):
-        math_string = "(anime.popularity + rec_anime.popularity)"
-        select_rec_score_string += f".0 * {math_string}, anime_recs.recommendations * {math_string}"
+        math_string = f"({media_type}.popularity + rec_{media_type}.popularity)"
+        select_rec_score_string += f".0 * {math_string}, {media_type}_recs.recommendations * {math_string}"
     else:
-        select_rec_score_string += f", anime_recs.recommendations"
+        select_rec_score_string += f", {media_type}_recs.recommendations"
 
     # If score compensation is enabled add it to the equation
     if request.GET.get("score_compensation"):
         # If there is no score given for an anime just use the average score for compensation
-        select_rec_score_string += f" * IIF(user_anime.score = 0, {user.model.average_anime_score}, user_anime.score)"
+        select_rec_score_string += (
+            f" * IIF(user_{media_type}.score = 0, {user.model.average_anime_score}, user_{media_type}.score)"
+        )
 
     # Close up the string
     select_rec_score_string += "),"
@@ -84,35 +90,36 @@ def json_response(request: HttpRequest) -> HttpResponse:
     # Building the SQL query by hand to make it as fast as possible
     # This is by far the slowest part of the website so it needs to be as fast as possible
     sql_command = f"""  SELECT
-                            anime_recs.recommended_media_id, 
-                            anime_recs.media_id, 
+                            {media_type}_recs.recommended_media_id, 
+                            {media_type}_recs.media_id, 
                             {select_rec_score_string}
-                            rec_anime.main_picture_medium, 
-                            rec_anime.title, 
-                            anime.title, 
+                            rec_{media_type}.main_picture_medium, 
+                            rec_{media_type}.title, 
+                            {media_type}.title, 
                             -- Convert watching string to be more readable
-                            rec_user_anime.status
-                        FROM user_anime
+                            rec_user_{media_type}.status
+                        FROM user_{media_type}
 
-                        -- Get information for the anime they have watched
-                        INNER JOIN anime ON (user_anime.media_id = anime.id)\n"""
+                        -- Get information for the {media_type} they have watched
+                        INNER JOIN {media_type} ON (user_{media_type}.media_id = {media_type}.id)\n"""
 
     if minimum_recs := request.GET.get("minimum_recs"):
-        sql_command += f""" -- Get recs for this anime
-                            INNER JOIN anime_recs ON (anime_recs.media_id = anime.id AND anime_recs.recommendations >= {minimum_recs})"""
+        sql_command += f""" -- Get recs for this {media_type}
+                            INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = {media_type}.id AND {media_type}_recs.recommendations >= {minimum_recs})"""
     else:
-        sql_command += """  -- Get recs for this anime
-                            INNER JOIN anime_recs ON (anime_recs.media_id = anime.id)"""
+        sql_command += f"""  -- Get recs for this {media_type}
+                            INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = {media_type}.id)"""
 
     sql_command += f""" -- Get rec information
-                        INNER JOIN anime rec_anime ON (anime_recs.recommended_media_id = rec_anime.id)
+                        INNER JOIN {media_type} rec_{media_type} ON ({media_type}_recs.recommended_media_id = rec_{media_type}.id)
 
                         -- Get status for the recs
-                        LEFT JOIN user_anime rec_user_anime ON (anime_recs.recommended_media_id = rec_user_anime.media_id AND rec_user_anime.user_id = {user.model.id})
+                        LEFT JOIN user_{media_type} rec_user_{media_type} ON ({media_type}_recs.recommended_media_id = rec_user_{media_type}.media_id AND rec_user_{media_type}.user_id = {user.model.id})
                         
                         -- Only the specific media type for this user
-                        WHERE ({status_in} AND user_anime.user_id = {user.model.id} {return_string}) """
-
+                        WHERE ({status_in} AND user_{media_type}.user_id = {user.model.id} {return_string}) """
+    print(sql_command)
+    print("============================================")
     # Add sql command to the command list
     command += ["-sql", sql_command]
 
