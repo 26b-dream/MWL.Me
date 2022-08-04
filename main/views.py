207891e6e0fd85701f743058fe38230d44404d1e
26b-dream <84663588+26b-dream@users.noreply.github.com>
2022-08-04 00:54:09 -0700

    New and improved database interaction style

--- main/views.py
+++ main/views.py
@@ -1,13 +1,16 @@
 import subprocess
+import time
 from datetime import datetime, timedelta
 from subprocess import PIPE
+from typing import Optional
 
+from django.db.models.query import prefetch_related_objects
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import redirect, render
 from django.utils.http import urlencode
 
 from common.myanimelist_user import MyAnimeListUser
-from main.models import ImportQue
+from main.models import Anime, AnimeRecs, ImportQue, UserAnime
 
 from .forms import NameForm
 
@@ -33,109 +36,116 @@ def index(request: HttpRequest) -> HttpResponse:
     return render(request, "main/index.html", {"form": form})
 
 
-def json_response(request: HttpRequest) -> HttpResponse:
-    form = NameForm(request.GET)
-    # TODO: Make this an actual 404-like page
-    if not form.is_valid():
-        return HttpResponse("Invalid form")
-
-    media_type = form.cleaned_data["anime_or_manga"]
-
-    command: list[str] = ["out"]
-    user = MyAnimeListUser(form.cleaned_data["username"])
-
+def status_in(form: NameForm, media_type: str) -> str:
     use_values = [value for key, value in USE_CROSSREF.items() if form.cleaned_data.get(key) == True]
-    status_in = f"""user_{media_type}.status IN ("{'", "'.join(use_values)}'")"""
+    return f"""user_{media_type}.status IN ("{'", "'.join(use_values)}'")"""
 
+
+def do_not_return_in(form: NameForm, media_type: str) -> str:
     do_not_return_values = [v for k, v in DO_NOT_RETURN_CROSSREF.items() if not form.cleaned_data.get(k)]
     # If anime/manga with a specific status are set to not be returned use a more complex query
     if do_not_return_values:
         return_string = '", "'.join(do_not_return_values)
         if form.cleaned_data.get("do_not_return_not_on_list"):
-            return_string = (
+            return (
                 f'AND (rec_user_{media_type}.status IS NULL or rec_user_{media_type}.status NOT IN ("{return_string}"))'
             )
         else:
-            return_string = f'AND (rec_user_{media_type}.status NOT IN ("{return_string}"))'
+            return f'AND (rec_user_{media_type}.status NOT IN ("{return_string}"))'
 
     # If all anime/manga are to be returned a simpler query can be used
     else:
-        return_string = ""
+        return ""
 
-    # If rec number needs to be truncated get the value
-    # When no value is given default to 10,000 which should never be reached
+
+def select_score_rec(form: NameForm, media_type: str, user: MyAnimeListUser) -> str:
     recs_to_truncate = form.cleaned_data["ignore_recs_over"]
-
     # TODO: Technically not every query needs the IIF statement
     # TODO: Including it just makes things easier to read and write
     # TODO: Find a clean way to split this up better for faster queries
     select_rec_score_string = f"IIF({media_type}_recs.recommendations > {recs_to_truncate}, {recs_to_truncate}"
     # If popularity compensation is enabled use an extra math string to compensate
-    if request.GET.get("popularity_compensation"):
+    # If popularity compensation is enabled use an extra math string to compensate
+    if form.cleaned_data["popularity_compensation"]:
         math_string = f"({media_type}.popularity + rec_{media_type}.popularity)"
         select_rec_score_string += f".0 * {math_string}, {media_type}_recs.recommendations * {math_string}"
     else:
         select_rec_score_string += f", {media_type}_recs.recommendations"
 
     # If score compensation is enabled add it to the equation
-    if request.GET.get("score_compensation"):
+    if form.cleaned_data["score_compensation"]:
         # If there is no score given for an anime just use the average score for compensation
         select_rec_score_string += (
             f" * IIF(user_{media_type}.score = 0, {user.model.average_anime_score}, user_{media_type}.score)"
         )
 
     # Close up the string
-    select_rec_score_string += "),"
+    select_rec_score_string += ")"
+    return select_rec_score_string
+
+
+def recs_string(form: NameForm, media_type: str) -> str:
+    if (minimum_recs := form.cleaned_data["minimum_recs"]) != "1":
+        return f"""INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = user_{media_type}.media_id AND {media_type}_recs.recommendations >= {minimum_recs})"""
+    else:
+        return f""" INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = user_{media_type}.media_id)"""
+
+
+def join_media_if_needed(form: NameForm, media_type: str) -> str:
+    if form.cleaned_data["popularity_compensation"]:
+        return f"""INNER JOIN {media_type} ON ({media_type}.id = user_{media_type}.media_id) INNER JOIN {media_type} rec_anime ON (rec_{media_type}.id = {media_type}_recs.media_id)"""
+    else:
+        return ""
+
+
+def json_response(request: HttpRequest) -> HttpResponse:
+    form = NameForm(request.GET)
+    # TODO: Make this an actual 404-like page
+    if not form.is_valid():
+        return HttpResponse("Invalid form")
+
+    command: list[str] = ["out"]  # Start of command to run external programm
+    media_type = form.cleaned_data["anime_or_manga"]
+    user = MyAnimeListUser(form.cleaned_data["username"])
 
     # Building the SQL query by hand to make it as fast as possible
     # This is by far the slowest part of the website so it needs to be as fast as possible
-    sql_command = f"""  SELECT
-                            {media_type}_recs.recommended_media_id, 
-                            {media_type}_recs.media_id, 
-                            {select_rec_score_string}
-                            rec_{media_type}.main_picture_medium, 
-                            rec_{media_type}.title, 
-                            {media_type}.title, 
-                            -- Convert watching string to be more readable
-                            rec_user_{media_type}.status
-                        FROM user_{media_type}
+    sql_command = f"""SELECT
+                        {media_type}_recs.recommended_media_id,
+                        SUM({select_score_rec(form, media_type, user)}) AS rec_score,
+                        rec_user_{media_type}.status,
+                        GROUP_CONCAT({media_type}_recs.media_id, CHAR(0x1F)) AS rec_ids,
+                        GROUP_CONCAT({media_type}_recs.recommendations, CHAR(0x1F)) AS rec_counts
 
-                        -- Get information for the {media_type} they have watched
-                        INNER JOIN {media_type} ON (user_{media_type}.media_id = {media_type}.id)\n"""
+                    -- Start with all entries on the user's list
+                    FROM user_{media_type}
+                    
+                    -- Get recommendations based on the media on the user's list
+                    {recs_string(form, media_type)}
 
-    if minimum_recs := request.GET.get("minimum_recs"):
-        sql_command += f""" -- Get recs for this {media_type}
-                            INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = {media_type}.id AND {media_type}_recs.recommendations >= {minimum_recs})"""
-    else:
-        sql_command += f"""  -- Get recs for this {media_type}
-                            INNER JOIN {media_type}_recs ON ({media_type}_recs.media_id = {media_type}.id)"""
+                    {join_media_if_needed(form, media_type)}
 
-    sql_command += f""" -- Get rec information
-                        INNER JOIN {media_type} rec_{media_type} ON ({media_type}_recs.recommended_media_id = rec_{media_type}.id)
+                    -- Must be a left join because not all entries will be on the uer's list
+                    LEFT JOIN user_{media_type} rec_user_{media_type} ON ({media_type}_recs.recommended_media_id = rec_user_{media_type}.media_id AND rec_user_{media_type}.user_id = {user.model.id})
 
-                        -- Get status for the recs
-                        LEFT JOIN user_{media_type} rec_user_{media_type} ON ({media_type}_recs.recommended_media_id = rec_user_{media_type}.media_id AND rec_user_{media_type}.user_id = {user.model.id})
-                        
-                        -- Only the specific media type for this user
-                        WHERE ({status_in} AND user_{media_type}.user_id = {user.model.id} {return_string}) """
-    print(sql_command)
-    print("============================================")
+                    -- Only the specific media type for this user
+                    WHERE ({status_in(form, media_type)} AND user_{media_type}.user_id = {user.model.id} {do_not_return_in(form, media_type)})
+                    
+                    GROUP BY {media_type}_recs.recommended_media_id
+                    ORDER BY rec_score DESC
+                    LIMIT {form.cleaned_data['number_of_results']}
+                        """
+
     # Add sql command to the command list
-    command += ["-sql", sql_command]
+    command = ["out", "-sql", sql_command]
 
-    # Add the number of results limiter to the command
-    command += ["-number_of_results", str(form.cleaned_data["number_of_results"])]
-
-    # If score compensation is enabled add it to the command
-    if form.cleaned_data["score_compensation"]:
-        command.append("score_compensation")
-
+    command.append("-debug")
+    print(sql_command)
     jsoned = subprocess.run(command, stdout=PIPE, stderr=PIPE).stdout.decode()
     return HttpResponse(jsoned)
 
 
 # TODO: Special response when dumb user disables all possible entries to use
-# This is "fast enough" in the future I may try to offload some of this into a faster language
 def recommendations(request: HttpRequest) -> HttpResponse:
     form = NameForm(request.GET)
     # TODO: Make this an actual 404-like page
